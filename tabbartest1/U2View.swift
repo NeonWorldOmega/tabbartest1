0a4e7b07eaeedcdd7c227b83d2b4d7a8790007a7
Brandon Fink <bfink88@gmail.com>
2023-07-29 03:42:36 -0400

    git

---
+++ tabbartest1/U2View.swift
@@ -0,0 +1,20 @@
+//
+//  U2View.swift
+//  tabbartest1
+//
+//  Created by Brandon Fink on 7/9/23.
+//
+
+import SwiftUI
+
+struct U2View: View {
+    var body: some View {
+        Text("U2")
+    }
+}
+
+struct U2View_Previews: PreviewProvider {
+    static var previews: some View {
+        U2View()
+    }
+}
